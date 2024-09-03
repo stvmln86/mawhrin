04453138c52138da97e4202133d19058098db395
Stephen Malone <mail@wirehaiku.org>
2024-09-03 16:47:31 +1000

    Added root modules; fixed requirements.

---
+++ mawhrin/__init__.py
@@ -0,0 +1,11 @@
+"""
+Package definition for 'mawhrin'.
+"""
+
+VERSION_DATE = "2024-09-03"
+VERSION_NUMS = "0.0.0"
+VERSION_TEXT = f"Mawhrin version {VERSION_NUMS} ({VERSION_DATE})."
+
+# from mawhrin import comms
+# from mawhrin import items
+# from mawhrin import tools
