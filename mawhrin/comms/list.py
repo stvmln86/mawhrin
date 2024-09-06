0f2b3631c6e2c04fa41c9ab34d5e609e3e5ab312
Stephen Malone <mail@wirehaiku.org>
2024-09-06 21:01:22 +1000

    Improved command long & short help. Fixes #8.

--- mawhrin/comms/list.py
+++ mawhrin/comms/list.py
@@ -8,14 +8,14 @@ from mawhrin.comms.base import group
 from mawhrin.items import Book
 
 
-@group.command(name="list")
-@click.argument("TEXT", default="")
+@group.command(name="list", short_help="List all notes.")
+@click.argument("NAME", default="")
 @click.pass_obj
-def list_(book: Book, text: str):
+def list_(book: Book, name: str):
     """
-    List all notes, or notes with names containing TEXT.
+    List all notes, or notes matching NAME.
     """
 
     for note in book:
-        if note.match(text):
+        if note.match(name):
             click.echo(note.name)
--- mawhrin/comms/show.py
+++ mawhrin/comms/show.py
@@ -8,12 +8,12 @@ from mawhrin.comms.base import group
 from mawhrin.items import Book
 
 
-@group.command()
-@click.argument("NAME")
+@group.command(short_help="Print an existing note.")
+@click.argument("name", metavar="NOTE")
 @click.pass_obj
 def show(book: Book, name: str):
     """
-    Print the contents of an existing note.
+    Print the contents of NOTE, if it exists.
     """
 
     if note := book.get(name):
