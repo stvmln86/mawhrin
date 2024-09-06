b44678c51a8885a271b41b2aa70a33f0a8cd84e6
Stephen Malone <mail@wirehaiku.org>
2024-09-07 07:39:55 +1000

    Fixed show command help.

--- mawhrin/comms/show.py
+++ mawhrin/comms/show.py
@@ -8,12 +8,12 @@ from mawhrin.comms.base import group
 from mawhrin.items import Book
 
 
-@group.command(short_help="Print an existing note.")
+@group.command(short_help="Print a note.")
 @click.argument("name", metavar="NOTE")
 @click.pass_obj
 def show(book: Book, name: str):
     """
-    Print the contents of NOTE, if it exists.
+    Print NOTE, if it exists.
     """
 
     if note := book.get(name):
